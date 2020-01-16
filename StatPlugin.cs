2951d9f8c03ef36b7a8f6cf57f4861126abfadb9
ISNE610 <oatsuetrong@gmail.com>
2020-01-16 14:02:45 +0700

    Do homework#1

--- StatPlugin.cs
+++ StatPlugin.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace DNWS
 {
@@ -29,13 +30,19 @@ namespace DNWS
     }
     public HTTPResponse GetResponse(HTTPRequest request)
     {
+      String[] ip = Regex.Split(request.getPropertyByKey("RemoteEndPoint"), ":");
       HTTPResponse response = null;
       StringBuilder sb = new StringBuilder();
-      sb.Append("<html><body><h1>Stat:</h1>");
+      sb.Append("<html><body>");
       foreach (KeyValuePair<String, int> entry in statDictionary)
-      {
-        sb.Append(entry.Key + ": " + entry.Value.ToString() + "<br />");
-      }
+      // {
+      //   sb.Append(entry.Key + ": " + entry.Value.ToString() + "<br />");
+      // }
+      sb.Append("Client IP: ").Append(ip[0]).Append("<br><br>");
+      sb.Append("Client Port: ").Append(ip[1]).Append("<br><br>");
+      sb.Append("Browser Information: ").Append(request.getPropertyByKey("User-Agent")).Append("<br><br>");
+      sb.Append("Accept Language: ").Append(request.getPropertyByKey("Accept-Language")).Append("<br><br>");
+      sb.Append("Accept Encoding: ").Append(request.getPropertyByKey("Accept-Encoding")).Append("<br><br>");
       sb.Append("</body></html>");
       response = new HTTPResponse(200);
       response.body = Encoding.UTF8.GetBytes(sb.ToString());
