dd1f93323389a5f8eb248e9af5d2c23034fe1788
ISNE610 <oatsuetrong@gmail.com>
2020-02-24 13:41:10 +0700

    Add thread

--- Program.cs
+++ Program.cs
@@ -296,7 +296,8 @@ namespace DNWS
                     // Get one, show some info
                     _parent.Log("Client accepted:" + clientSocket.RemoteEndPoint.ToString());
                     HTTPProcessor hp = new HTTPProcessor(clientSocket, _parent);
-                    hp.Process();
+                    Thread t = new Thread(new ThreadStart(hp.Process));
+                    t.Start();
                 }
                 catch (Exception ex)
                 {
